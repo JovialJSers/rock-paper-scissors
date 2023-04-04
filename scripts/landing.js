69cd017d0d60bc85009e4fa03577ade0a762148c
jcannon04 <123973436+jcannon04@users.noreply.github.com>
2023-04-03 19:09:56 -0500

    fixed relative path for github pages

--- scripts/landing.js
+++ scripts/landing.js
@@ -3,12 +3,12 @@ const buttonContainer = document.getElementById('button-container');
 buttonContainer.addEventListener('click', function (event) {
   if (event.target.id === 'pvp') {
     localStorage.setItem('game-mode', 'pvp');
-    location.href = '../pages/characters.html';
+    location.href = './pages/characters.html';
     return;
   }
   if (event.target.id === 'pvc') {
     localStorage.setItem('game-mode', 'pvc');
-    location.href = '../pages/characters.html';
+    location.href = './pages/characters.html';
     return;
   }
 });
